12221e11d6e6fbce31bbb37f9b4bc3cab08d18bb
Hack Reactor Students <adrice727@gmail.com>
2014-05-12 17:07:20 -0700

    Initial commit

--- app/config.js
+++ app/config.js
@@ -45,5 +45,17 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 // Add additional schema definitions below
 /************************************************************/
 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.string('username').primary();
+      user.string('salt', 255);
+      user.string('password', 255);
+    }).then(function (table) {
+      console.log('Created Users Table', table);
+    });
+  }
+});
+
 
 module.exports = db;
--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,34 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
+var userCheck = function(username) {
+  return new Promise(function(resolve, reject) {
+    db.knex('users').where('username', '=', username);
+  });
+};
+
+
 var User = db.Model.extend({
+  tableName: 'users',
+  links: function(){
+    return this.hasMany(Link);
+  },
+  initialize: function() {
+    this.on('creating', function(model, attrs, options){
+      var username = attrs.username;
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(attrs.password, salt);
+
+      userCheck()
+      .then(function(user) {
+        // re-direct to login, if user found
+      }).catch(function(e){
+        // create a new user in the database
+
+      });
+    });
+  }
 });
 
 module.exports = User;
+
